99740986e9fa3a596b0de62aa9d267fe052acf21
zadmin88 <zaimon88@gmail.com>
2023-03-09 16:00:57 -0500

    build frontend

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true
+    content: 'HTML is easy',
+    date: '2022-01-10T17:30:31.098Z',
+    important: true,
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false
+    content: 'Browser can execute only Javascript',
+    date: '2022-01-10T18:39:34.091Z',
+    important: false,
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true
-  }
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2022-01-10T19:20:14.298Z',
+    important: true,
+  },
 ]
 
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -29,9 +32,7 @@ app.get('/', (req, res) => {
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
   return maxId + 1
 }
 
@@ -39,8 +40,8 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+    return response.status(400).json({
+      error: 'content missing',
     })
   }
 
@@ -62,14 +63,14 @@ app.get('/api/notes', (req, res) => {
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  notes = notes.filter((note) => note.id !== id)
 
   response.status(204).end()
 })
 
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+  const note = notes.find((note) => note.id === id)
 
   if (note) {
     response.json(note)
@@ -78,7 +79,7 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
